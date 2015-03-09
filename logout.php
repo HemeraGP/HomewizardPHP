b814feaeb9b53ef5df71926fa8cad2973ee87f3c
Guy Verschuere <guy@egregius.be>
2015-03-09 18:04:49 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
- <?php
-require("common.php");
-unset($_SESSION['user']);
-unset($_COOKIE['HomewizardPHP']);
-setcookie("HomewizardPHP", null, time()-1);
-header("Location: index.php");
-die("Redirecting to: index.php"); 
