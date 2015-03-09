1af19bcffdf09299dc3adeec5acf4d15426cb080
Guy Verschuere <guy@egregius.be>
2015-03-09 18:06:32 +0100

    Delete parameters.php

--- parameters.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username = "homewizard";
-$password = "home!wizard";
-$host = "localhost";
-$dbname = "homewizard";
-$db = new mysqli($host, $username, $password, $dbname);
-if($db->connect_errno > 0){ echo('Unable to connect to database [' . $db->connect_error . ']');}
